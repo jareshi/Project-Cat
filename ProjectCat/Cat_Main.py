c16453c558363950a436a2bc287bd275eb98809e
Jaberul Haque <h.jaberul2020@gmail.com>
2024-05-17 21:24:12 -0400

    [Dev] Added main program

---
+++ ProjectCat/Cat_Main.py
@@ -0,0 +1,16 @@
+# This will be the main file that will be used to run the game.
+
+# Importing the LLM generation function
+from Cat_AI import LLM_generation
+
+print("""
+        _                ___       _.--.
+        \`.|\..----...-'`   `-._.-'_.-'`
+        /  ' `         ,       __.--'
+        )/' _/     \   `-_,   /
+        `-'" `"\_  ,_.-;_.-\_ ',     
+            _.-'_./   {_.'   ; /
+           {_.-``-'         {_/ 
+    """)
+
+print("Welcome to ")
