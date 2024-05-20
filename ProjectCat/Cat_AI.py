369a120a66594ede12af63ed12c2fbba7c78aa0a
Jaberul Haque <h.jaberul2020@gmail.com>
2024-05-19 21:16:33 -0400

    Added comments to Cat_AI.py for better understanding

--- ProjectCat/Cat_AI.py
+++ ProjectCat/Cat_AI.py
@@ -14,9 +14,22 @@ from Cat_Main import adventurer
 from monsters import elementalDragon
 
 def LLM_generation(adventurerHistory, adventurerQuery):
-    try:
-        localModel = Ollama(model = "mistral") # Sets the model as Mistral-7b-Instruct-v0.2
+    """
+    This function uses the Ollama model to generate paths for the adventurer to follow based
+    on the decisions that they make. It also sets the rules for the AI to follow.
 
+    Parameters:
+    adventurerHistory (list): List of the adventurer's previous actions
+    adventurerQuery (str): The adventurer's current query
+
+    Returns:
+    str: The AI's response to the adventurer's query
+    """
+    try:
+        # Set the model to use, in this case, Mistral-7b-Instruct-v0.2
+        localModel = Ollama(model = "mistral")
+
+        # Separate this from the template so that the variables can be dynamically set
         adventureText = f"""
         You are now the guide of an epic journey in the region of Wolfspine.
         An adventurer named {adventurer.name}, who is a {adventurer.characterClass},
@@ -24,22 +37,26 @@ def LLM_generation(adventurerHistory, adventurerQuery):
         You must navigate the {adventurer.name} through challenges, choices, battles,
         and consequences, dynamically adapting the tale based on the adventurer's
         decisions. Your goal is to create a branching narrative experience where each
-        choice leads to a new path, ultimately determining {adventurer.name}'s fate.\n
+        choice leads to a new path, ultimately determining {adventurer.name}'s fate.
         """
         
+        # Create the adventure template that will be used by the AI to generate responses
         adventureTemplate = adventureText + """
         Here are some rules to follow:
         1. Have a few paths that lead to success
-        2. Have a few paths that lead to death. If the adventurer dies generate a response that explains the death and ends in the text: "The End.", I will search for this text to end the game
-        3. There are only a few monsters that can be encountered, they are the following: goblin, fire drake, ogre, giant spider, and shadow mage, during an encounter, generate a response that says "You have encountered a..." and include the name of one of the aforementioned monsters, I will search for this text to start a battle
-        4. If the adventurer's health reaches 0, generate a response that explains the death and ends in the text: "The End.", I will search for this text to end the game
+        2. Have a few paths that lead to death. If the adventurer dies, generate a response that explains the death and ends in the text: "The End". We will search for this text to end the game.
+        3. There are only a few monsters that can be encountered, they are the following: goblin, fire drake, ogre, giant spider, and shadow mage. During an encounter, generate a response that says "You have encountered a..." and include the name of one of the aforementioned monsters. We will search for this text to start a battle.
+        4. If the adventurer's health reaches 0, generate a response that explains the death and ends in the text: "The End". We will search for this text to end the game.
         
         Here is the journey so far, use this to understand what to say next: {history}
         Human: {query}
         AI: 
         """
 
+        # Create a prompt template using the adventure template
         prompt = ChatPromptTemplate.from_template(adventureTemplate)
+
+        # Create a chain of operations that will be applied to the adventurer's query
         chain = (
             {"history": adventurerHistory, "query": RunnablePassthrough()}
             | prompt
@@ -47,9 +64,9 @@ def LLM_generation(adventurerHistory, adventurerQuery):
             | StrOutputParser()
         )
 
+        # Invoke the chain and return the AI's response
         return(chain.invoke(adventurerQuery))
-    
+   
     except Exception as e:
-        # If there is an error, an error message will be displayed
-        # This is for debugging purposes, and will be removed later
+        # If there is an error, print the error message
         print(f"There was an error in LLM_generation:\n{e}")
